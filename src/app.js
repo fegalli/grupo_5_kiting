100452bb4284e32da47f2e8b9eb35c41885792c4
fegalli <federicoegalli@gmail.com>
2021-02-20 14:41:22 -0300

    implementar el moduo de ejs, renombrar todas las vistas actuales para que utilicen la extension .ejs, modificar los metodos de los controladores para que utilicen el metodo render

--- src/app.js
+++ src/app.js
@@ -7,7 +7,10 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 
 // Llamado de rutas
 const router = require('../src/routers/router');
- 
 app.use('/', router);
-
 app.listen(process.env.PORT || '3000', () => console.log('Servidor corriendo en el puerto 3000'))
+
+// Configurando EJS
+app.set('view engine', 'ejs');  // Aca le estoy diciendo a express que voy a utilizar un motor de plantillas y que ese motor es ejs
+app.set('views', './src/views'); // con esta linea le digo a express en que carpeta estan mis vistas. Si no la utizo, por default express ira a buscar una carpeta que se llame -views-
+    //PRO-TIP el metodo .set() permite definir configuraciones de express
--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -2,19 +2,20 @@ const path = require('path');
 
 module.exports = {
     index : (req, res)=>{
-        return res.sendFile(path.resolve(__dirname,"../views/home.html"))
+        return res.render('home')
+        // return res.sendFile(path.resolve(__dirname,"../views/home.ejs")) Lo dejo como ejemplo de como se resuelve las vistas si no usamos el motor de plantilla .ejs (cuando usabamos directamente el html)
     },
     login : (req,res)=>{
-        return res.sendFile(path.resolve(__dirname,'../views/login.html'))
+        return res.render('login')
     },
     register : (req, res)=>{
-        return res.sendFile(path.resolve(__dirname,'../views/register.html'))
+        return res.render('register')
     },
     productCart : (req, res)=>{
-        return res.sendFile(path.resolve(__dirname,'../views/productCart.html'))
+        return res.render('productCart')
     },
     productDetail : (req, res)=>{
-        return res.sendFile(path.resolve(__dirname,'../views/productDetail.html'))
+        return res.render('productDetail')
     }
 }
  
