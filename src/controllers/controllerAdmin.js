60286a046c6e9b7ca45d5a49deb7fb6142763e9a
fegalli <federicoegalli@gmail.com>
2021-04-25 19:19:08 -0300

    CRUD hecho aunque falta debug

--- src/controllers/controllerAdmin.js
+++ src/controllers/controllerAdmin.js
@@ -20,70 +20,52 @@ module.exports = {
         css: "/admin/productsCreate.css"})
     },
     // Accion de crear un producto
-    
-  
-    // Accion de crear un producto
     productsSave : (req,res) => {
         db.Product.create({
+            id : 1, // hago esto para tratar saltear el error "Field 'id' doesn't have a default value"
             price    : req.body.price,
             comments : req.body.comments,
-            nameId   : req.body.name,
-            sizeId   : req.body.size,
-            brandId  : req.body.brand,
-            styleId  : req.body.style 
+            nameId   :  req.body.name,
+            sizeId   :  req.body.size,
+            brandId  :  req.body.brand,
+            styleId  :  req.body.style 
         });
         res.redirect('/admin/create')
     },
     // Se muestra el formulario de edicion de un producto
-    productEdit : (req, res) => {
-        let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json')))
-        let miProducto = productos.find(producto => {
-                if(producto.id == req.params.id ){
-                    return producto
-                }         
-        });
-        return res.render ("./admin/productEdit",{
-            css:"/admin/productEdit.css",
-            miProducto
-        })
+    productEdit: (req,res) =>{
+        db.Product.findByPk(req.params.id)
+            .then(product=>{
+                return res.render ("./admin/productEdit",{
+                    css:"/admin/productEdit.css",
+                    product
+                })
+            })
     },
-    // accion de editar un producto //
-    productUpdate : (req,res)=>{
-        // 1 - me traigo todos los productos
-        let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json')))   
-        // 2 - identifico mi producto
-        let miProducto = productos.find(producto => {
-            if(producto.id == req.params.id ){
-                return producto
-            }})
-        // 3 - identifico mi producto en la lista de productos y escribo sus nusvos valores
-        req.body.id = req.params.id;
-        let productosUpdate = productos.map(producto =>{
-            if(producto.id == miProducto.id){
-                return req.body
+    // accion de editar un producto 
+    productUpdate : (req,res) => {
+        db.Product.update({
+            id : 1, // hago esto para tratar saltear el error "Field 'id' doesn't have a default value"
+            price    : req.body.price,
+            comments : req.body.comments,
+            nameId   :  req.body.name,
+            sizeId   :  req.body.size,
+            brandId  :  req.body.brand,
+            styleId  :  req.body.style 
+        },{
+            where : {
+                id : req.params.id
             }
-            return producto
-        })
-        // 4 - subo el nuevo json de productos
-        let productosUpdateJSON = JSON.stringify(productosUpdate, null, 2)
-        fs.writeFileSync(path.resolve(__dirname,'../data/products.json'),productosUpdateJSON)
-        return res.redirect('/admin/products')
-    },
-    // Se el detalle de un producto en particular
-    productsShow : (req, res) => {
-        let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json')))
-        let miProducto = productos.find(producto => {
-                if(producto.id == req.params.id ){
-                    return producto
-                }         
         });
-        res.send(miProducto)
-    },
+        res.redirect('/admin/create')
+    }, 
+    // Se el detalle de un producto en particular
+    // Mismo comportamiento que CRUD con json. Se debe mejorar
     productsShow : (req,res) => {
-        db.Product.findByPk()
+        db.Product.findByPk(req.params.id)
             .then((product)=>{
                 res.json(product)
-            })
+            })  
     },
     // Accion de eliminar un producto
     destroy : (req,res) =>{
@@ -107,6 +89,8 @@ module.exports = {
 //         productos
 //     })
 // },
+
+// Accion de crear un producto
 // productsSave : (req,res) => {
     //     let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json')))
     //     let ultimoProducto = productos.pop()
@@ -127,7 +111,56 @@ module.exports = {
     //     fs.writeFileSync(path.resolve(__dirname,'../data/products.json'),nuevoProductoJson)
         
     // },
-        // destroy : (req, res) => {
+
+    // Se muestra el formulario de edicion de un producto
+    // productEdit : (req, res) => {
+    //     let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json')))
+    //     let miProducto = productos.find(producto => {
+    //             if(producto.id == req.params.id ){
+    //                 return producto
+    //             }         
+    //     });
+    //     return res.render ("./admin/productEdit",{
+    //         css:"/admin/productEdit.css",
+    //         miProducto
+    //     })
+    // },
+
+        // Se el detalle de un producto en particular
+    // productsShow : (req, res) => {
+    //     let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json')))
+    //     let miProducto = productos.find(producto => {
+    //             if(producto.id == req.params.id ){
+    //                 return producto
+    //             }         
+    //     });
+    //     res.json(miProducto)
+    // },
+
+    // accion de editar un producto 
+    // productUpdate : (req,res)=>{
+    //     // 1 - me traigo todos los productos
+    //     let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json')))   
+    //     // 2 - identifico mi producto
+    //     let miProducto = productos.find(producto => {
+    //         if(producto.id == req.params.id ){
+    //             return producto
+    //         }})
+    //     // 3 - identifico mi producto en la lista de productos y escribo sus nusvos valores
+    //     req.body.id = req.params.id;
+    //     let productosUpdate = productos.map(producto =>{
+    //         if(producto.id == miProducto.id){
+    //             return req.body
+    //         }
+    //         return producto
+    //     })
+    //     // 4 - subo el nuevo json de productos
+    //     let productosUpdateJSON = JSON.stringify(productosUpdate, null, 2)
+    //     fs.writeFileSync(path.resolve(__dirname,'../data/products.json'),productosUpdateJSON)
+    //     return res.redirect('/admin/products')
+    // },
+
+    // destroy : (req, res) => {
     //     let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/products.json')))
     //     const productoDeleteId = req.params.id;
     //     const productosFinal = productos.filter(productos => productos.id != productoDeleteId);
@@ -135,6 +168,7 @@ module.exports = {
     //     fs.writeFileSync(path.resolve(__dirname, '../data/products.json'),productosFinalJSON);
     //     return res.redirect('/admin/products') //res.send("El producto ha sido borrado exitosamente");
     // },
+
 
 
 
